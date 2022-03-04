28cee49a9b0b64c946c67a804d68588e60e90f0c
Roman <mamchiy@yandex.ru>
2022-03-05 01:16:29 +0300

    update task1_Lesson2

--- Python_client_server_App/Lesson-2/task1.py
+++ Python_client_server_App/Lesson-2/task1.py
@@ -2,7 +2,6 @@
 1. Задание на закрепление знаний по модулю CSV. Написать скрипт, осуществляющий выборку определенных данных из
 файлов info_1.txt, info_2.txt, info_3.txt и формирующий новый «отчетный» файл в формате CSV.
 Для этого:
-
 Создать функцию get_data(), в которой в цикле осуществляется перебор файлов с данными, их открытие и считывание
 данных. В этой функции из считанных данных необходимо с помощью регулярных выражений извлечь значения параметров
 «Изготовитель системы», «Название ОС», «Код продукта», «Тип системы».
@@ -16,53 +15,53 @@
 получение данных через вызов функции get_data(), а также сохранение подготовленных данных в
 соответствующий CSV-файл; Проверить работу программы через вызов функции write_to_csv().
 """
+
+import csv
 import re
+import chardet
 
-from chardet import detect
 
-
-# Вводные данные
-input_files = ['info_1.txt', 'info_2.txt', 'info_3.txt']
-list_encoding = []  # список кодировки файлов
-os_prod_list = []  # «Изготовитель системы»
-os_name_list = []  # список «Название ОС»
-os_code_list = []  # список «Код продукта»
-os_type_list = []  # список «Тип системы»
+#  Вводные данные (список файлов, в неизвестной кодировке)
+list_input_files = ['info_1.txt', 'info_2.txt', 'info_3.txt']
 
 
 def get_data(files):
     """
-    Функция, которая определяет кодировку файлов, читает файл и дополнительно возвращает
-    список кодировок.
+    Функция, которая получаем из аргумента files список файлов, определяет для каждого файла кодировку, читает
+    файл и с помощью регулярных выражений извлекает значения параметров для переменной main_data.
     :param files:
-    :return: list_encoding
+    :return: main_data
     """
-
+    main_data = [['Изготовитель системы', 'Название ОС', 'Код продукта', 'Тип системы']]
     for file in files:
-        # узнаём кодировку неизвестного нам файла
         with open(file, 'rb') as f:
             content = f.read()
-        encoding = detect(content)['encoding']
-        print('encoding: ', encoding)
-        list_encoding.append(encoding)
-
-        # открываем файл в известной нам кодировке
-        with open(file, encoding=encoding) as f_n:
-            for el_str in f_n:
-                print(el_str, end='')
-            print()
-            print('-' * 70)
-
-            # result = []
-            # for line in f_n.readiness():
-            #     result += re.findall(r'^(\w[^:]+).*:\s+([^:\n]+)\s*$', line)
-            #     print(result)
+        content = content.decode(chardet.detect(content)['encoding'])
+        os_prod_list = file, re.findall(r'Изготовитель системы:\s+(\w+)', content)[0].strip()
+        os_name_list = file, re.findall(r'Название ОС:\s+([\w.\s]+)\n', content)[0].strip()
+        os_code_list = file, re.findall(r'Код продукта:\s+([\w\-]+\w+)', content)[0].strip()
+        os_type_list = file, re.findall(r'Тип системы:\s+([\w\-\w\s\w]+)', content)[0].strip()
+        main_data.extend([[os_prod_list[1], os_name_list[1], os_code_list[1], os_type_list[1]]])
+        print(main_data)
+    return main_data
 
 
-def write_to_csv():
-    pass
+def write_to_csv(csv_file, input_files):
+    """
+    Функция, которая извлекает значения параметров из неизвестных файлов в неизвестной кодировке
+    и сохраняет данные в указанном в аргументе файле.
+    :param csv_file:
+    :param input_files:
+    :return:
+    """
+
+    to_write = get_data(input_files)
+    with open(csv_file, 'w', encoding='utf-8', newline="") as main_data_file:
+        md_writer = csv.writer(main_data_file, quoting=csv.QUOTE_ALL, quotechar="\"")
+        md_writer.writerow(to_write[0])
+        md_writer.writerows(to_write[1:], )
+    return print(f'Данные записаны в файл: {csv_file}')
 
 
 if __name__ == "__main__":
-    get_data(input_files)
-    # print(list_encoding)
+    write_to_csv('main.csv', list_input_files)
